7e9affae2737db5455f507b3258296467fa0ad8a
averheim <markus.averheim@gmail.com>
2017-09-04 14:37:06 +0200

    client creation implemented

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,60 @@
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import config.AccountConfig;
+import config.ConfigReader;
+
+import java.util.List;
 
 public class Application {
+
+    private ConfigReader configReader;
+    private List<AccountConfig> accounts;
+    private AmazonS3 s3;
+    private AmazonEC2 ec2;
+    private TagInterpreter tagInterpreter;
+    private EventInterpreter eventInterpreter;
+
+    public Application() {
+        this.configReader = new ConfigReader();
+        this.accounts = configReader.readConfig();
+    }
+
+    public void run(){
+        for (AccountConfig account : accounts) {
+            for (String region : account.getRegions()) {
+                createAmazonClients(account, region);
+                instantiateClasses();
+
+            }
+
+        }
+
+    }
+
+    private void instantiateClasses() {
+        this.tagInterpreter = new TagInterpreter(ec2);
+        this.eventInterpreter = new EventInterpreter(ec2, tagInterpreter);
+
+    }
+
+    private void createAmazonClients(AccountConfig account, String region) {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(account.getAccessKeyId(),
+                account.getSecretAccessKey());
+
+        this.s3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .withRegion(Regions.fromName(region))
+                .build();
+
+        this.ec2 = AmazonEC2ClientBuilder.standard()
+                .withRegion(region)
+                .build();
+    }
+
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,9 @@
-import config.AccountConfig;
-import config.ConfigReader;
-
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ConfigReader configReader = new ConfigReader();
-        List<AccountConfig> accounts = configReader.readConfig();
-        for (AccountConfig account : accounts) {
-            System.out.println(account.getAccessKeyId());
-        }
-
+        new Application().run();
 
     }
 }
