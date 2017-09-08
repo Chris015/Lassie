6d49ce9c32aeab508faec2e2da6f8e72416675f9
chris015 <christopher.olsson@hyperisland.se>
2017-09-08 14:01:06 +0200

    Made ResourceTaggerFactory case-insensetive

--- src/main/java/lassie/resourcetagger/ResourceTaggerFactory.java
+++ src/main/java/lassie/resourcetagger/ResourceTaggerFactory.java
@@ -3,10 +3,10 @@ package lassie.resourcetagger;
 public class ResourceTaggerFactory {
 
     public ResourceTagger getResourceTagger(String resourceType) throws UnsupportedResourceTypeException {
-        switch (resourceType) {
-            case "RunInstances": return new RunInstancesTagger();
-            case "CreateSecurityGroup": return new CreateSecurityGroupTagger();
-            case "S3Bucket": return new S3BucketTagger();
+        switch (resourceType.toUpperCase()) {
+            case "RUNINSTANCES": return new RunInstancesTagger();
+            case "CREATESECURITYGROUP": return new CreateSecurityGroupTagger();
+            case "S3BUCKET": return new S3BucketTagger();
         }
         throw new UnsupportedResourceTypeException(resourceType + " is not a supported");
     }
