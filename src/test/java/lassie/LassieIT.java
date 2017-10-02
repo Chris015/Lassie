afbaaac3aa75f8351f708cd4c4e7d5c7d84da14c
chris015 <christopher.olsson@hyperisland.se>
2017-10-02 08:24:19 +0200

    added IT for security groups and volumes

--- src/test/java/lassie/LassieIT.java
+++ src/test/java/lassie/LassieIT.java
@@ -1,25 +1,28 @@
 package lassie;
 
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.SecurityGroup;
+import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.Volume;
 import lassie.awshandlers.Ec2Handler;
 import lassie.config.Account;
 import lassie.mocks.ConfigReaderMock;
 import lassie.mocks.EC2HandlerMock;
 import lassie.mocks.LogFetcherMock;
 import lassie.model.Log;
-import lassie.resourcetagger.EC2InstanceTagger;
 import lassie.resourcetagger.ResourceTaggerFactory;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+import static java.util.Collections.*;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class LassieIT {
+    private static final String OWNER_TAG = "Owner";
+    private String[] args = new String[0];
     private Application application;
     private Ec2Handler ec2Handler;
 
@@ -34,54 +37,104 @@ public class LassieIT {
     }
 
     @Test
-    public void oneUntaggedInstance_invokesTagResourcesOnce() throws Exception {
+    public void untaggedInstances_areTagged() throws Exception {
         //given
-        prepareTaggingEc2InstancesTest();
+        prepareEc2Test("ec2instance", "ec2Instances.json");
 
-        List<String> ids = new ArrayList<>();
-        ids.add("i-06e9aaf9760467624");
-        EC2HandlerMock.idsForInstancesWithoutTags = ids;
+        HashMap<String, Instance> instances = EC2HandlerMock.instances;
+        Instance instance = instances.get("i-07a5bbg4326310341");
+        assertEquals(0, instance.getTags().size());
+
+        instance = instances.get("i-06e9aaf9760467624");
+        assertEquals(1, instance.getTags().size());
 
         //when
-        application.run(new String[0]);
+        application.run(args);
 
         //then
         verify(ec2Handler, times(1))
-                .tagResource("i-06e9aaf9760467624", "Owner", "john.doe");
+                .tagResource("i-07a5bbg4326310341", OWNER_TAG, "jane.doe");
         verify(ec2Handler, times(0))
-                .tagResource("i-07a5bbg4326310341", "Owner", "jane.doe");
+                .tagResource("i-06e9aaf9760467624", OWNER_TAG, "john.doe");
+
+        assertEquals(1, instance.getTags().size());
+
+        Tag tag = instances.get("i-07a5bbg4326310341").getTags().get(0);
+        assertEquals(OWNER_TAG, tag.getKey());
+        assertEquals("jane.doe", tag.getValue());
     }
 
     @Test
-    public void twoUntaggedInstances_invokesTagResourcesTwice() throws Exception {
+    public void untaggedSecurityGroups_areTagged() throws Exception {
         //given
-        prepareTaggingEc2InstancesTest();
+        prepareEc2Test("securitygroup", "securitygroups.json");
 
-        List<String> ids = new ArrayList<>();
-        ids.add("i-06e9aaf9760467624");
-        ids.add("i-07a5bbg4326310341");
-        EC2HandlerMock.idsForInstancesWithoutTags = ids;
+        HashMap<String, SecurityGroup> securityGroups = EC2HandlerMock.securityGroups;
+        SecurityGroup securityGroup = securityGroups.get("s-9831ba2f192s2");
+        assertEquals(1, securityGroup.getTags().size());
+
+        securityGroup = securityGroups.get("s-1214cb1l323b1");
+        assertEquals(0, securityGroup.getTags().size());
 
         //when
-        application.run(new String[0]);
+        application.run(args);
 
         //then
         verify(ec2Handler, times(1))
-                .tagResource("i-06e9aaf9760467624", "Owner", "john.doe");
+                .tagResource("s-1214cb1l323b1", OWNER_TAG, "johnny.doe");
+        verify(ec2Handler, times(0))
+                .tagResource("s-9831ba2f192s2", OWNER_TAG, "jane.doe");
 
-        verify(ec2Handler, times(1))
-                .tagResource("i-07a5bbg4326310341", "Owner", "jane.doe");
+        Tag tag = securityGroups.get("s-1214cb1l323b1").getTags().get(0);
+        assertEquals(OWNER_TAG, tag.getKey());
+        assertEquals("johnny.doe", tag.getValue());
     }
 
-    private void prepareTaggingEc2InstancesTest() {
+    @Test
+    public void untaggedVolumes_areTagged() throws Exception {
+        //given
+        prepareEc2Test("ebsvolume", "ebsvolumes.json");
+
+        HashMap<String, Volume> volumes = EC2HandlerMock.volumes;
+        Volume volume = volumes.get("v-203412c121a31");
+        assertEquals(0, volume.getTags().size());
+
+        volume = volumes.get("v-313821c242b32");
+        assertEquals(0, volume.getTags().size());
+        assertEquals(1, volume.getAttachments().size());
+
+        volume = volumes.get("v-109812b123a21");
+        assertEquals(1, volume.getTags().size());
+
+        //when
+        application.run(args);
+
+        //then
+        verify(ec2Handler, times(1))
+                .tagResource("v-203412c121a31", OWNER_TAG, "johnny.doe");
+        verify(ec2Handler, times(1))
+                .tagResource("v-313821c242b32", OWNER_TAG, "john.doe");
+        verify(ec2Handler, times(0))
+                .tagResource("v-109812b123a21", OWNER_TAG, "jane.doe");
+
+        Tag tag = volumes.get("v-203412c121a31").getTags().get(0);
+        assertEquals(OWNER_TAG, tag.getKey());
+        assertEquals("johnny.doe", tag.getValue());
+
+        tag = volumes.get("v-313821c242b32").getTags().get(0);
+        assertEquals(OWNER_TAG, tag.getKey());
+        assertEquals("john.doe", tag.getValue());
+    }
+
+    private void prepareEc2Test(String resourceType, String fileName) {
         List<Account> accounts = new ArrayList<>();
-        Account account = createAccount(Arrays.asList("ec2instance"));
+        Account account = createAccount(singletonList(resourceType));
         accounts.add(account);
 
         ConfigReaderMock.accounts = accounts;
 
         List<String> filePaths = new ArrayList<>();
-        filePaths.add(ClassLoader.getSystemResource("ec2Instances.json").getPath());
+        filePaths.add(ClassLoader.getSystemResource(fileName).getPath());
 
         List<Log> logs = new ArrayList<>();
         Log log = new Log(account, filePaths);
@@ -93,8 +146,8 @@ public class LassieIT {
         Account account = new Account();
         account.setSecretAccessKey("");
         account.setAccessKeyId("");
-        account.setOwnerTag("Owner");
-        account.setRegions(Arrays.asList(""));
+        account.setOwnerTag(OWNER_TAG);
+        account.setRegions(singletonList(""));
         account.setResourceTypes(resourceTypes);
         return account;
     }
